2d11e2be74df8fb64d9e709a9bd20dee80adf874
Sandesha <sandesha.shetty@ramanacoachingcenter.com>
2019-10-26 17:35:28 -0400

    sample commit

--- server/src/main/java/com/ssdi/project/bookExchange/BookExchangeApplication.java
+++ server/src/main/java/com/ssdi/project/bookExchange/BookExchangeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookExchangeApplication {
-
+	//Sample commit by Sandesha Shetty
 	public static void main(String[] args) {
 		SpringApplication.run(BookExchangeApplication.class, args);
 	}
