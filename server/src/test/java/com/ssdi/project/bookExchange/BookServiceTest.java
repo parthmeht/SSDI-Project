07324e2800523a4a5d74edbe16d3e8b3922300b5
mpugh6 <mpugh6@uncc.edu>
2019-11-12 12:47:02 -0500

    UI update and test file changed

--- server/src/test/java/com/ssdi/project/bookExchange/BookServiceTest.java
+++ server/src/test/java/com/ssdi/project/bookExchange/BookServiceTest.java
@@ -31,14 +31,18 @@ public class BookServiceTest {
         initMocks(this);
         bookServiceUnderTest = new BookService(mockBookRepository);
         List<Book> books = new ArrayList<>();
+        List<Book> searchResults = new ArrayList<>();
         Book book1 = Book.builder().id(1).author("J K Rowling").title("Harry Potter").isListed(true).price(25.99).build();
         Book book2 = Book.builder().id(1).author("Joli Jensen").title("Write No Matter What: Advice for Academics").isListed(true).price(29.99).build();
         Book book3 = Book.builder().id(1).author("Thomas H. Cormen, Charles E. Leiserson, Ronald Rivest, Clifford Stein").title("Introduction to Algorithms").isListed(false).price(99.99).build();
         books.add(book1);
         books.add(book2);
         books.add(book3);
+        searchResults.add(book1);
         Mockito.when(mockBookRepository.findAll())
                 .thenReturn(books);
+        Mockito.when(mockBookRepository.searchBooks("Harry Potter"))
+                .thenReturn(searchResults);
     }
 
     @Test
@@ -51,6 +55,18 @@ public class BookServiceTest {
 
         assertNotEquals(99.99, books.get(1).getPrice());
         assertNotEquals(true, books.get(2).getIsListed());
+    }
+
+    @Test
+    public void testBookSearch() {
+        // Setup
+        final String title = "Harry Potter";
+
+        // Run the test
+        final List<Book> result = bookServiceUnderTest.searchBooks(title);
+
+        // Verify the results
+        assertEquals(title, result.get(0).getTitle());
     }
 
 }
