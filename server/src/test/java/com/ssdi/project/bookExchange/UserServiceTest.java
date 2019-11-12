161c0856a39802ba53b1b3dc14b4835271f0ce93
Parth Mehta <parthmeht@gmail.com>
2019-11-12 11:12:03 -0500

    changed user test cases

--- server/src/test/java/com/ssdi/project/bookExchange/UserServiceTest.java
+++ server/src/test/java/com/ssdi/project/bookExchange/UserServiceTest.java
@@ -1,8 +1,9 @@
-package com.ssdi.project.bookExchange.service;
+package com.ssdi.project.bookExchange;
 
 import com.ssdi.project.bookExchange.model.User;
 import com.ssdi.project.bookExchange.repository.RoleRepository;
 import com.ssdi.project.bookExchange.repository.UserRepository;
+import com.ssdi.project.bookExchange.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,15 +11,16 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-@ActiveProfiles("test")
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTest {
     @Mock
@@ -37,12 +39,28 @@ public class UserServiceTest {
         userServiceUnderTest = new UserService(mockUserRepository,
                 mockRoleRepository,
                 mockBCryptPasswordEncoder);
+        List<User> users = new ArrayList<>();
         user = User.builder()
                 .id(1)
                 .name("Gustavo")
                 .lastName("Ponce")
                 .email("test@test.com")
                 .build();
+        User user1 = User.builder()
+                .id(2)
+                .name("Parth")
+                .lastName("Mehta")
+                .email("testParth@test.com")
+                .build();
+        User user2 = User.builder()
+                .id(2)
+                .name("Rohit")
+                .lastName("Sharma")
+                .email("rohitSharma@test.com")
+                .build();
+        users.add(user);
+        users.add(user1);
+        users.add(user2);
 
         Mockito.when(mockUserRepository.save(any()))
                 .thenReturn(user);
@@ -50,41 +68,41 @@ public class UserServiceTest {
                 .thenReturn(user);
         Mockito.when(mockUserRepository.getById(anyInt()))
                 .thenReturn(user);
+        Mockito.when(mockUserRepository.findAll())
+                .thenReturn(users);
     }
 
     @Test
     public void testFindUserByEmail() {
-        // Setup
         final String email = "test@test.com";
-
-        // Run the test
         final User result = userServiceUnderTest.findUserByEmail(email);
-
-        // Verify the results
         assertEquals(email, result.getEmail());
     }
 
     @Test
     public void testGetUserById() {
-        // Setup
         final int id = 1;
-
-        // Run the test
         final User result = userServiceUnderTest.getUserById(id);
-
-        // Verify the results
         assertEquals(id, result.getId());
     }
 
     @Test
     public void testSaveUser() {
-        // Setup
         final String email = "test@test.com";
-
-        // Run the test
-        User result = userServiceUnderTest.saveUser(User.builder().build());
-
-        // Verify the results
+        User result = userServiceUnderTest.saveUser(user);
         assertEquals(email, result.getEmail());
+    }
+
+    @Test
+    public void testListAll(){
+        List<User> al = userServiceUnderTest.listAll();
+
+        assertEquals(true,al.size()>0);
+        assertEquals("Gustavo",al.get(0).getName());
+        assertEquals("Parth",al.get(1).getName());
+        assertEquals("Rohit",al.get(2).getName());
+
+        assertNotEquals("test@test.com", al.get(1).getEmail());
+        assertNotEquals("test@test.com", al.get(2).getEmail());
     }
 }
